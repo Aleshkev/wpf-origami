5d423fbecb2f209a0afcaedd92e92e2127970a71
Aleshkev <43296251+Aleshkev@users.noreply.github.com>
2021-11-09 00:53:02 +0100

    Finally passes the tests

--- origami.ml
+++ origami.ml
@@ -5,39 +5,47 @@ type kartka = point -> int
 
 open Complex
 
-let debug s t =
-  print_endline @@ (Printf.sprintf "%10s" s) ^ "  "
-  ^ String.concat "  "
-      (List.map (fun x -> Printf.sprintf "%7.2f %7.2f" x.re x.im) t)
+let eps = 1e-11
 
-let as_complex (a, b) = { re = a; im = b }
-let as_point p : point = (p.re, p.im)
+(* Używamy wewnętrznie liczb zespolonych, bo są takimi wektorami
+   zaimplementowanymi w bibliotece standardowej. Upraszczają obliczenia
+   geometryczne. *)
+let complex_of (a, b) = { re = a; im = b }
+let point_of p : point = (p.re, p.im)
 let ( +.. ), ( -.. ), ( *.. ), ( /.. ) = (add, sub, mul, div)
 let cross_product a b = (a.re *. b.im) -. (a.im *. b.re)
 
-let reflect p v =
-  let r = conj (v /.. p) *.. p in
-  debug "reflect" [ p; v; r ];
-  r
+type side = Left | Line | Right
 
-let prostokat p q v =
-  let ( <=.. ) (a, b) (c, d) = a <= c && b <= d in
-  debug "prostokat" [ as_complex p; as_complex q; as_complex v ];
-  if p <=.. v && v <=.. q then 1 else 0
+(** Po której stronie prostej wyznaczonej przez [p], [q] leży punkt [v]. *)
+let relate p q v =
+  match cross_product (q -.. p) (v -.. p) with
+  | x when abs_float x < eps -> Line
+  | x when x < 0. -> Right
+  | _ -> Left
+
+(** Wektor symetryczny do wektora [v] względem wektora [p]. *)
+let reflect_free p v = conj (v /.. p) *.. p
+
+(** Punkt symetryczny do [v] względem prostej wyznaczonej przez [p], [q]. *)
+let reflect p q v = p +.. reflect_free (q -.. p) (v -.. p)
+
+let ( <=.. ) (a, b) (c, d) = a <= c && b <= d
+let prostokat p q v = if p <=.. v && v <=.. q then 1 else 0
 
 let kolko o r v =
-  let o = as_complex o and v = as_complex v in
-  if norm2 (o -.. v) <= r ** 2. then 1 else 0
+  let o = complex_of o and v = complex_of v in
+  (* Dodajemy [eps] żeby po straceniu odrobinki precyzji punkciki na krawędzi
+     nadal należały do koła. *)
+  if norm (o -.. v) <= r +. eps then 1 else 0
 
 let zloz p q f v =
-  let q = as_complex q and p = as_complex p and v = as_complex v in
-  debug "zloz" [ p; q; v ];
-  match cross_product (q -.. p) (v -.. p) with
-  | x when x < 0. -> 0
-  | x when abs_float x < 1e-11 -> f (as_point v)
-  | _ ->
-      let v' = p +.. reflect (q -.. p) (v -.. p) in
-      debug "zloz v'" [ p; q; v; v' ];
-      f (as_point v) + f (as_point v')
+  let p = complex_of p and q = complex_of q and v = complex_of v in
+  match relate p q v with
+  | Line -> f (point_of v)
+  | Right -> 0
+  | Left ->
+      let v' = reflect p q v in
+      f (point_of v) + f (point_of v')
 
 let skladaj l k = List.fold_left (fun k (p, q) -> zloz p q k) k l
