fcc7f4173a1e6baae9fc04b3dd7cd27ec9da811c
Aleshkev <43296251+Aleshkev@users.noreply.github.com>
2021-11-09 17:03:30 +0100

    Reformat

--- origami.ml
+++ origami.ml
@@ -3,15 +3,20 @@
 type point = float * float
 type kartka = point -> int
 
-open Complex
+let ( <=.. ) (a, b) (c, d) = a <= c && b <= d
+let prostokat p q v = if p <=.. v && v <=.. q then 1 else 0
 
 (* Używamy wewnętrznie liczb zespolonych, bo są takimi wektorami
    zaimplementowanymi w bibliotece standardowej. Upraszczają obliczenia
    geometryczne. *)
+open Complex
+
 let complex_of (a, b) = { re = a; im = b }
-let point_of p : point = (p.re, p.im)
-let ( +.. ), ( -.. ), ( *.. ), ( /.. ) = (add, sub, mul, div)
+let point_of p : point = p.re, p.im
+let ( +.. ), ( -.. ), ( *.. ), ( /.. ) = add, sub, mul, div
 let cross_product a b = (a.re *. b.im) -. (a.im *. b.re)
+
+(** Floaty nie są dokładne, więc trzeba tolerować czasami małe błędy. *)
 let eps = 1e-11
 
 type side = Left | Right | Neither
@@ -29,9 +34,6 @@ let reflect_free p v = conj (v /.. p) *.. p
 (** Punkt symetryczny do [v] względem prostej wyznaczonej przez [p, q]. *)
 let reflect (p, q) v = p +.. reflect_free (q -.. p) (v -.. p)
 
-let ( <=.. ) (a, b) (c, d) = a <= c && b <= d
-let prostokat p q v = if p <=.. v && v <=.. q then 1 else 0
-
 let kolko o r v =
   let o = complex_of o and v = complex_of v in
   (* Zwiększamy troszeczkę średnicę żeby po straceniu odrobinki precyzji
@@ -39,7 +41,7 @@ let kolko o r v =
   if norm (o -.. v) <= r +. eps then 1 else 0
 
 let zloz p q f v =
-  let pq = (complex_of p, complex_of q) and v = complex_of v in
+  let pq = complex_of p, complex_of q and v = complex_of v in
   match relate pq v with
   | Left ->
       let v' = reflect pq v in
